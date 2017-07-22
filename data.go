cd3a586d9a95f4be342c324d64cd2213e0463579
Giulio Iotti <dullgiulio@gmail.com>
2017-07-22 11:27:59 +0300

    main: remove influxdb support; always verbose

--- data.go
+++ data.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
 	"time"
 )
 
@@ -43,118 +37,20 @@ func (r *result) String() string {
 		r.name, r.point, r.value, r.time.UnixNano())
 }
 
-type collector interface {
-	collect(<-chan *result)
-}
-
-type batchCollector struct {
-	endpoint string
-	nbatch   int
-	batchi   int // current position in batch slice
-	tbatch   time.Duration
-	batch    []*result
-	client   *http.Client
-}
-
-func newBatchCollector(endp string, nbatch int, tbatch time.Duration, client *http.Client) *batchCollector {
-	return &batchCollector{
-		endpoint: endp,
-		nbatch:   nbatch,
-		tbatch:   tbatch,
-		client:   client,
-		batch:    make([]*result, nbatch),
-	}
-}
-
-func (b *batchCollector) collect(ch <-chan *result) {
-	var skipTick bool // avoid flushing because of full and then timeout
-	tick := time.Tick(b.tbatch)
-	for {
-		select {
-		case res := <-ch:
-			if b.batchi >= b.nbatch {
-				b.flush()
-				skipTick = true
-			}
-			b.batch[b.batchi] = res
-			b.batchi++
-		case <-tick:
-			if skipTick {
-				skipTick = false
-				continue
-			}
-			b.flush()
-		}
-	}
-}
-
-func (b *batchCollector) flush() {
-	var buf bytes.Buffer
-	for i := 0; i < b.batchi; i++ {
-		fmt.Fprintln(&buf, b.batch[i].String())
-		b.batch[i] = nil
-	}
-	b.batchi = 0
-	resp, err := b.client.Post(b.endpoint, "text/plain", &buf)
-	if err != nil {
-		log.Printf("influxdb: error posting data: %s", err)
-		return
-	}
-	if resp.StatusCode != 204 {
-		log.Printf("influxdb: error posting data: expected status 204, got %s", resp.Status)
-	}
-	io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close()
-}
-
-func ProxyAwareHttpClient() (*http.Client, error) {
-	proxyurl, ok := os.LookupEnv("HTTP_PROXY")
-	if !ok {
-		return http.DefaultClient, nil
-	}
-	purl, err := url.Parse(proxyurl)
-	if err != nil {
-		return nil, fmt.Errorf("invalid URL in environment variable HTTP_PROXY: %s", err)
-	}
-	fmt.Printf("%#v\n", purl)
-	tr := &http.Transport{
-		Proxy: http.ProxyURL(purl),
-	}
-	return &http.Client{Transport: tr}, nil
-}
-
-type printCollector struct {
-	w io.Writer
-}
-
-func (p printCollector) collect(ch <-chan *result) {
-	for r := range ch {
-		fmt.Fprintln(p.w, r.String())
-	}
-}
-
 type results struct {
-	sinks []chan *result
-	ch    chan *result
+	w  io.Writer
+	ch chan *result
 }
 
-func newResults(cols []collector) *results {
-	r := &results{
-		sinks: make([]chan *result, len(cols)),
-		ch:    make(chan *result),
+func newResults(w io.Writer) *results {
+	return &results{
+		w:  w,
+		ch: make(chan *result),
 	}
-	for i := range cols {
-		ch := make(chan *result)
-		r.sinks[i] = ch
-		go cols[i].collect(ch)
-	}
-	return r
 }
 
 func (r *results) collect() {
 	for res := range r.ch {
-		for i := range r.sinks {
-			r.sinks[i] <- res
-		}
+		fmt.Fprintln(r.w, res.String())
 	}
 }
--- main.go
+++ main.go
@@ -15,10 +15,6 @@ func main() {
 	dsnmap := dsnmap(make(map[string]*dsnentry))
 	flag.Var(dsnmap, "mysql", "Named DSN to connect to database")
 	nworkers := flag.Int("workers", 1, "Number of parallel task runners")
-	verbose := flag.Bool("verbose", false, "Print measurements to stdout")
-	influxdb := flag.String("influxdb", defaultInfluxURL, "Address of InfluxDB write endpoint")
-	nbatch := flag.Int("influx-nbatch", 20, "Max number of measurements to cache")
-	tbatch := flag.Duration("influx-batch-time", 10*time.Second, "Max duration betweek flushes of InfluxDB cache")
 	every := flag.Duration("default-every", 10*time.Second, "Default repetition duration for tasks that don't specify the 'every' flag")
 	flag.Parse()
 
@@ -35,19 +31,7 @@ func main() {
 		}
 	}
 
-	var collectors []collector
-	if *influxdb != "" && *influxdb != defaultInfluxURL {
-		client, err := ProxyAwareHttpClient()
-		if err != nil {
-			log.Fatalf("fatal: %s", err)
-		}
-		collectors = append(collectors, newBatchCollector(*influxdb, *nbatch, *tbatch, client))
-	}
-	if *verbose {
-		collectors = append(collectors, printCollector{os.Stdout})
-	}
-
-	results := newResults(collectors)
+	results := newResults(os.Stdout)
 	ts, err := initProducts(hostname, *every, prods, dsnmap)
 	if err != nil {
 		log.Fatalf("error: %s", err)
